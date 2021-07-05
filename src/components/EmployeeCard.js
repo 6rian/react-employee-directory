f12fc3b5252c3753b223b6944d6ba5e689967394
Brian Griffin <wbgriffin@pm.me>
2021-07-04 23:41:03 -0400

    Added mock data

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -4,15 +4,15 @@ function EmployeeCard(props) {
   return (
     <div className="employee-card">
       <div className="employee-picture">
-        <img src="https://placeimg.com/300/300/any" className="responsive-img circle-img" alt="John Doe" />
+        <img src={props.pictureUrl} className="responsive-img circle-img" alt="John Doe" />
       </div>
       <div className="employee-info">
-        <span className="name">John Doe</span>
-        <span className="dept">Engineering</span>
-        <span className="title">Front End Developer</span>
-        <span className="email">john.doe@company.com</span>
-        <span className="phone">999.123.4567</span>
-        <span className="location">Brooklyn, NY</span>
+        <span className="name">{props.firstName} {props.lastName}</span>
+        <span className="dept">{props.department}</span>
+        <span className="title">{props.title}</span>
+        <span className="email">{props.email}</span>
+        <span className="phone">{props.phone}</span>
+        <span className="location">{props.location}</span>
       </div>
     </div>
   );
--- src/components/EmployeeDirectory.js
+++ src/components/EmployeeDirectory.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import DisplayOptions from './DisplayOptions';
 import EmployeeCard from './EmployeeCard';
+import employeesData from '../data/employees.json';
 
-function EmployeeDirectory(props) {
-  return (
-    <div className="employee-directory">
-      <DisplayOptions />
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
-    </div>
-  );
+class EmployeeDirectory extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      employees: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      employees: employeesData
+    });
+  }
+
+  render() {
+    const employeeCards = employeesData.results.map(employee => {
+      return (
+        <EmployeeCard
+          pictureUrl={employee.picture.medium}
+          firstName={employee.name.first}
+          lastName={employee.name.last}
+          email={employee.email}
+          phone={employee.phone}
+          location={`${employee.location.city}, ${employee.location.state}`}
+          title="Front End Developer"
+          department="Engineering"
+        />
+      );
+    });
+
+    return(
+      <div className = "employee-directory" >
+        <DisplayOptions />
+        {employeeCards}
+      </div>
+    );
+  }
 }
 
 export default EmployeeDirectory;
