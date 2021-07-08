0837a324abec5050983d9a864eea3f7dc1a8e88f
Brian Griffin <wbgriffin@pm.me>
2021-07-08 09:34:15 -0400

    Style updates for EmployeeCards

--- client/src/components/EmployeeCard.js
+++ client/src/components/EmployeeCard.js
@@ -7,12 +7,30 @@ function EmployeeCard(props) {
         <img src={props.pictureUrl} className="responsive-img circle-img" alt="John Doe" />
       </div>
       <div className="employee-info">
-        <span className="name">{props.firstName} {props.lastName}</span>
-        <span className="email">{props.email}</span>
-        <span className="title">{props.title}</span>
-        <span className="phone">{props.phone}</span>
-        <span className="dept">{props.department}</span>
-        <span className="location">{props.location}</span>
+        <span className="name">
+          <small>Name:</small>
+          {props.firstName} {props.lastName}
+        </span>
+        <span className="email">
+          <small>Email:</small>
+          {props.email}
+        </span>
+        <span className="title">
+          <small>Title:</small>
+          {props.title}
+        </span>
+        <span className="phone">
+          <small>Phone:</small>
+          {props.phone}
+        </span>
+        <span className="dept">
+          <small>Department:</small>
+          {props.department}
+        </span>
+        <span className="location">
+          <small>Location:</small>
+          {props.location}
+        </span>
       </div>
     </div>
   );
